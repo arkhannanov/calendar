41ca70002dfc494eaa7ea8c2f2f1bb5998e644d5
Artur_Khannanov <arkhannanov@gmail.com>
2019-12-06 15:44:29 +0300

    refactoring

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import * as axios from "axios";
 
 export const instance = axios.create({
-    baseURL: 'https://calendar-server-arkhannanov.herokuapp.com/'
+    baseURL: 'http://localhost:5000/'
 });
 
